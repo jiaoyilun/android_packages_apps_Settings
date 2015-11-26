b6ee5c3202146cf8a3f9a7ce6870ab33aa2cb690
jiaoyilun <jiaoyilun@gmail.com>
2015-11-26 11:17:11 +0800

    commit
    Change-Id: I3c5234a736c9d2c1a21f22c13a9b2853449d827f

--- src/com/android/settings/bliss/RecentsSettings.java
+++ src/com/android/settings/bliss/RecentsSettings.java
@@ -15,32 +15,46 @@
 */
 package com.android.settings.bliss;
 
-import android.content.Context;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceCategory;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
-import android.provider.Settings;
 import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.cyanogenmod.PackageListAdapter;
+import com.android.settings.cyanogenmod.PackageListAdapter.PackageItem;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
-import com.android.settings.SettingsPreferenceFragment;
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+public class RecentsSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener, Indexable,AdapterView.OnItemLongClickListener, OnPreferenceClickListener {
 
-public class RecentsSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener, Indexable {
-
+	static final String TAG = "RecentsSettings";
     private static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 	private static final String PREF_CLEAR_ALL_BG_COLOR =
@@ -56,6 +70,15 @@ public class RecentsSettings extends SettingsPreferenceFragment implements OnPre
     private ListPreference mRecentsClearAllLocation;
 //    private ColorPickerPreference mClearAllIconColor;
     private ColorPickerPreference mClearAllBgColor;
+    
+    private static final int DIALOG_APPS = 0;
+    private PackageListAdapter mPackageAdapter;
+    private PackageManager mPackageManager;
+    private PreferenceGroup mDndPrefList;
+    private Preference mAddDndPref;
+    private String mDndPackageList;
+    private Map<String, Package> mDndPackages;
+    
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -99,6 +122,15 @@ public class RecentsSettings extends SettingsPreferenceFragment implements OnPre
 //        mClearAllIconColor.setSummary(hexColor);
 //        mClearAllIconColor.setDefaultColors(WHITE, WHITE);
 //        mClearAllIconColor.setOnPreferenceChangeListener(this);
+        
+        mPackageManager = getPackageManager();
+        mPackageAdapter = new PackageListAdapter(getActivity());
+        
+        mDndPrefList = (PreferenceGroup) findPreference("exclude_applications_list");
+        mDndPrefList.setOrderingAsAdded(false);
+        mDndPackages = new HashMap<String, Package>();
+        mAddDndPref = findPreference("add_exclude_packages");
+        mAddDndPref.setOnPreferenceClickListener(this);
 
     }
 
@@ -192,4 +224,203 @@ public class RecentsSettings extends SettingsPreferenceFragment implements OnPre
             return result;
         }
     };
+
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+	    refreshCustomApplicationPrefs();
+        getListView().setOnItemLongClickListener(this);
+	}
+
+	@Override
+	public boolean onPreferenceClick(Preference preference) {
+		 if (preference == mAddDndPref) {
+	            showDialog(DIALOG_APPS);
+	        }
+		 return true;
+	}
+	
+	 /**
+     * Application class
+     */
+    private static class Package {
+        public String name;
+        /**
+         * Stores all the application values in one call
+         * @param name
+         */
+        public Package(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append(name);
+            return builder.toString();
+        }
+
+        public static Package fromString(String value) {
+            if (TextUtils.isEmpty(value)) {
+                return null;
+            }
+            try {
+                Package item = new Package(value);
+                return item;
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
+    }
+    
+    /**
+     * Utility classes and supporting methods
+     */
+    @Override
+    public Dialog onCreateDialog(int id) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final Dialog dialog;
+        final ListView list = new ListView(getActivity());
+        list.setAdapter(mPackageAdapter);
+
+        builder.setTitle(R.string.profile_choose_app);
+        builder.setView(list);
+        dialog = builder.create();
+
+        switch (id) {
+            case DIALOG_APPS:
+                list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent,
+                            View view, int position, long id) {
+                        PackageItem info = (PackageItem) parent.getItemAtPosition(position);
+                        addCustomApplicationPref(info.packageName, mDndPackages);
+                        dialog.cancel();
+                    }
+                });
+        }
+        return dialog;
+    }
+
+    private void addCustomApplicationPref(String packageName, Map<String,Package> map) {
+        Package pkg = map.get(packageName);
+        if (pkg == null) {
+            pkg = new Package(packageName);
+            map.put(packageName, pkg);
+            savePackageList(false, map);
+            refreshCustomApplicationPrefs();
+        }
+    }
+    
+    private void savePackageList(boolean preferencesUpdated, Map<String,Package> map) {
+        String setting = Settings.System.RECENTS_WHITE_VALUES;
+
+        List<String> settings = new ArrayList<String>();
+        for (Package app : map.values()) {
+            settings.add(app.toString());
+        }
+        final String value = TextUtils.join("|", settings);
+        if (preferencesUpdated) {
+                mDndPackageList = value;
+        }
+        Settings.System.putString(getContentResolver(), setting, value);
+    }
+    
+    private void refreshCustomApplicationPrefs() {
+        if (!parsePackageList()) {
+            return;
+        }
+        // Add the Application Preferences
+        if (mDndPrefList != null ) {
+            mDndPrefList.removeAll();
+            for (Package pkg : mDndPackages.values()) {
+                try {
+                    Preference pref = createPreferenceFromInfo(pkg);
+                    mDndPrefList.addPreference(pref);
+                } catch (PackageManager.NameNotFoundException e) {
+                }
+            }
+        }
+        mAddDndPref.setOrder(0);
+        mDndPrefList.addPreference(mAddDndPref);
+    }
+    
+    private boolean parsePackageList() {
+        boolean parsed = false;
+        final String dndString = Settings.System.getString(getContentResolver(),
+                Settings.System.RECENTS_WHITE_VALUES);
+        if (!TextUtils.equals(mDndPackageList, dndString)) {
+            mDndPackageList = dndString;
+            mDndPackages.clear();
+            parseAndAddToMap(dndString, mDndPackages);
+            parsed = true;
+        }
+        return parsed;
+    }
+    
+    private void parseAndAddToMap(String baseString, Map<String,Package> map) {
+        if (baseString == null) {
+            return;
+        }
+        final String[] array = TextUtils.split(baseString, "\\|");
+        for (String item : array) {
+            if (TextUtils.isEmpty(item)) {
+                continue;
+            }
+            Package pkg = Package.fromString(item);
+            map.put(pkg.name, pkg);
+        }
+    }
+    
+    private Preference createPreferenceFromInfo(Package pkg)
+            throws PackageManager.NameNotFoundException {
+        PackageInfo info = mPackageManager.getPackageInfo(pkg.name,
+                PackageManager.GET_META_DATA);
+        Preference pref = new Preference(getActivity());
+
+        pref.setKey(pkg.name);
+        pref.setTitle(info.applicationInfo.loadLabel(mPackageManager));
+        pref.setIcon(info.applicationInfo.loadIcon(mPackageManager));
+        pref.setPersistent(false);
+        return pref;
+    }
+
+	@Override
+	public boolean onItemLongClick(AdapterView<?> parent, View view,
+			int position, long id) {
+		final Preference pref =
+                (Preference) getPreferenceScreen().getRootAdapter().getItem(position);
+
+        if (mDndPrefList.findPreference(pref.getKey()) != pref) {
+            return false;
+        }
+
+        if (mAddDndPref == pref) {
+            return false;
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.dialog_delete_title)
+                .setMessage(R.string.dialog_delete_message)
+                .setIconAttribute(android.R.attr.alertDialogIcon)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                            removeApplicationPref(pref.getKey(), mDndPackages);
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, null);
+
+        builder.show();
+        return true;
+	}
+    
+	private void removeApplicationPref(String packageName, Map<String,Package> map) {
+        if (map.remove(packageName) != null) {
+            savePackageList(false, map);
+            refreshCustomApplicationPrefs();
+        }
+    }
+	
 }
